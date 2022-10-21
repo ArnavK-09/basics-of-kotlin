aebfc2fd5329a538116b555459c4b6a98d049134
Arnav K <arnavkaushal09@gmail.com>
2022-10-21 10:17:59 +0000

    Inputs

---
+++ 05-StandardInput/Main.kt
@@ -0,0 +1,19 @@
+// Get Input
+import java.util.Scanner; // from java
+
+// practical
+fun main() {
+    /* Standard Kotlin */
+    print("Enter some text1: ");
+    var input1 = readline();
+    print("\t You Entered: $input1\n\n")
+
+    /* Java Scanner */
+    print("Enter some text2: ");
+    var input2 = Scanner(System.`in`);
+    print("\t You Entered: $input2")
+}
+
+/*
+    NOTE - I HAVEN'T TRIED THIS, WILL CAUSE ERR PROBABLY
+*/
