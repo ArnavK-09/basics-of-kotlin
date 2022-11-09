243975f5a7efd6bd66335498b19714054bea7115
Arnav K <arnavkaushal09@gmail.com>
2022-11-09 14:55:02 +0530

    Functions

---
+++ 16-functions/Main.kt
@@ -0,0 +1,19 @@
+// Functions In Kotlin 
+fun main() {
+  // Simple sum function 
+  fun sum(a: Int, b: Int): Int {
+    return a + b;
+  }
+  print("Simple sum function:- ${sum(2,3)}");
+
+  /**
+      The list of different standard library functions and their use –
+
+      sqrt() – Used to calculate the square root of a number.
+      print() – Used to print a message to standard output.
+      rem() – To find the remainder of one number when divided by another.
+      toInt() – To convert a number to integer value.
+      readline() – Used for standard input.
+      compareTo() – To compare two numbers and return boolean value.
+   **/
+}
